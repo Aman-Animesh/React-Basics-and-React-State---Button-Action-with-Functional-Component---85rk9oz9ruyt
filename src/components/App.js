1bacb503ffda7fe6cd568ab4bd1c65955e19873d
Animesh Srivastava <animeshsrivastava6199501@gmail.com>
2022-04-11 20:11:44 +0530

    adding

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,15 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [para, setpara] = useState("")
+
+  const textChange= ()=>{
+    setpara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <p id="para">{para} </p>
+      <button onClick={textChange} id="click">Click</button>
     </div>
   );
 }
